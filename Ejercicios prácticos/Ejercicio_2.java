2547a1020154a4ba00dce54d76a7f872060bfa58
Pendder <penddergast@gmail.com>
2025-09-09 13:53:57 +0200

    Añadida carpeta de Ejercicios

---
+++ Ejercicios prácticos/Ejercicio_2.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Imp_06 - Mañana
+ */
+public class Ejercicio_2 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        
+        String frase;
+        String letra;
+        int contador=0;
+        
+        System.out.println("Escribe una frase");
+        frase=s.nextLine();
+        System.out.println("Ahora dime una letra");
+        letra=s.nextLine();
+        
+        for(int i=0;i<frase.length()-1;i++){
+            if(frase.charAt(i)==letra.charAt(0)){
+                contador++;
+            }
+        }
+        
+        System.out.println(contador);
+    }
+}
