f2fb7bac26e751eae36dbe8b12117d22dc421a79
Pendder <penddergast@gmail.com>
2025-10-14 12:56:24 +0200

    main;

--- Proyectos Curso/hospital/src/main/java/com/example/hospital/HospitalApplication.java
+++ Proyectos Curso/hospital/src/main/java/com/example/hospital/HospitalApplication.java
@@ -6,10 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HospitalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HospitalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalApplication.class, args);
 
 
+        UsuarioDAO dao = new UsuarioDAO();
 
+        // Crear
+        dao.crearUsuario(new Usuario(null, "Ana", 30));
+
+        // Leer
+        dao.obtenerUsuarios().forEach(u ->
+                System.out.println(u.getId() + " - " + u.getNombre() + " - " + u.getEdad())
+        );
+
+
+    }
 }
+
