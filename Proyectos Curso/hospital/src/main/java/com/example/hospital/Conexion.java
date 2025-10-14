352859c2b3f5615e0777c9d0f6f35c7ac03723c4
Pendergastt <penddergast@gmail.com>
2025-10-14 18:10:05 +0200

    crudMongo

--- Proyectos Curso/hospital/src/main/java/com/example/hospital/Conexion.java
+++ Proyectos Curso/hospital/src/main/java/com/example/hospital/Conexion.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class Conexion {
     private static final String URL = "jdbc:mysql://localhost:3306/hospital";
     private static final String USER = "root";
-    private static final String PASS = "root";
+    private static final String PASS = "Root";
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASS);
--- Proyectos Curso/hospital/src/main/java/com/example/hospital/controller/UsuarioController.java
+++ Proyectos Curso/hospital/src/main/java/com/example/hospital/controller/UsuarioController.java
@@ -1,18 +1,17 @@
 package com.example.hospital.controller;
 
 
+import com.example.hospital.Usuario;
 import com.example.hospital.UsuarioDAO;
 import org.bson.Document;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("/usuario")
 
 public class UsuarioController {
+
 
     @GetMapping("/buscar/{id}")
     public Document obtenerDocumentoPorId(@PathVariable String id) {
@@ -23,10 +22,33 @@ public class UsuarioController {
 
          return usuario;
 
+    }
 
+    @PostMapping("/agregar")
+    public void agregarUsuario(@RequestBody Usuario usuario){
+        UsuarioDAO dao = new UsuarioDAO();
+
+        dao.crearUsuario(usuario);
 
     }
-}
 
+    @PatchMapping("/modificar/{id}")
+    public void modUsuario(@PathVariable String id, @RequestBody Usuario modificacion){
+        UsuarioDAO dao = new UsuarioDAO();
+
+        dao.actualizarUsuario(id, modificacion.getNombre(), modificacion.getEdad());
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminarUsuario(@PathVariable String id){
+        UsuarioDAO dao = new UsuarioDAO();
+        dao.eliminarUsuario(id);
+
+    }
+
+
+
+
+}
 
 
