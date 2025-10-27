1cd19d56cd7b56e517be286efcf19ac65b63b45f
Pendder <penddergast@gmail.com>
2025-10-27 10:33:42 +0100

    ejercicio etiquetas

---
+++ VisualCode/Ejercicio CSS/script.js
@@ -0,0 +1,41 @@
+function numeroEnlacesPagina(){
+
+let numeroEnlaces = document.getElementsByTagName("a");
+alert(`El número de enlaces a la página es ${numeroEnlaces.length}`);
+
+}
+
+function direccionPenultimoEnlace(){
+    let enlaces = document.getElementsByTagName("a");
+let penultimo = (document.getElementsByTagName("a").length-2);
+alert(enlaces[penultimo]);
+
+}
+
+function enlacesAPrueba(){
+    let enlaces = document.getElementsByTagName("a");
+    let n=0;
+       
+
+    for (i=0; i<enlaces.length; i++){
+        if (enlaces[i].href == "http://prueba/"){
+            console.log(enlaces[i]);
+            n++
+        }
+    }
+
+    alert(`El número de enlaces que llevan a prueba es ${n}`);
+
+}
+
+function numeroEnlacesTercerParrafo(){
+
+let nParrafos= document.getElementsByTagName("p");
+let ultimoParrafo = nParrafos[nParrafos.length-1];
+let numeroEnlaces = ultimoParrafo.getElementsByTagName("a").length;
+alert(`En el tercer párrafo hay ${numeroEnlaces} enlaces`);
+
+
+
+
+}
